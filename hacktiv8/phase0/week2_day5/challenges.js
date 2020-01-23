bfc53c27dcefbb710fcb0bb0b105d97267b544d0
ideaspaper <agni.infinity@gmail.com>
2020-01-24 01:07:23 +0700

    update for challenge 7, Sorting!

--- hacktiv8/phase0/week2_day5/challenges.js
+++ hacktiv8/phase0/week2_day5/challenges.js
@@ -83,10 +83,10 @@ function sittingArrangement(person, column) {
   }
   return tempArr;
 }
-console.log(sittingArrangement(['A', 'B', 'C'], 0 )); //Invalid number
-console.log(sittingArrangement([ 'Juli', 'Nisa', 'Desi', 'Ulfa', 'Puji' ], 2)); //[ [ 'Juli', 'Nisa' ], [ 'Desi', 'Ulfa' ], [ 'Puji', 'Kursi Kosong' ] ]
-console.log(sittingArrangement([ 'Yosia', 'Asrawi', 'Andru' ], 3)); //[ [ 'Yosia', 'Asrawi', 'Andru' ] ]
-console.log(sittingArrangement([ 'Lukman', 'Adam', 'Dimas', 'Hansin', 'Orion' ], 4)); // [ [ 'Lukman', 'Adam', 'Dimas', 'Hansin' ], [ 'Orion', 'Kursi Kosong', 'Kursi Kosong', 'Kursi Kosong' ] ]
+console.log(sittingArrangement(['A', 'B', 'C'], 0)); //Invalid number
+console.log(sittingArrangement(['Juli', 'Nisa', 'Desi', 'Ulfa', 'Puji'], 2)); //[ [ 'Juli', 'Nisa' ], [ 'Desi', 'Ulfa' ], [ 'Puji', 'Kursi Kosong' ] ]
+console.log(sittingArrangement(['Yosia', 'Asrawi', 'Andru'], 3)); //[ [ 'Yosia', 'Asrawi', 'Andru' ] ]
+console.log(sittingArrangement(['Lukman', 'Adam', 'Dimas', 'Hansin', 'Orion'], 4)); // [ [ 'Lukman', 'Adam', 'Dimas', 'Hansin' ], [ 'Orion', 'Kursi Kosong', 'Kursi Kosong', 'Kursi Kosong' ] ]
 
 
 // Challenge 5
@@ -145,17 +145,62 @@ console.log(tukarBesarKecil('001-A-3-5TrdYW')); // "001-a-3-5tRDyw"
 console.log('\n');
 console.log('Sorting! challenge:');
 function sorting(array) {
-  // your code here
+  for (let i = 0; i < array.length; i++) {
+    let swapped = false;
+    for (let j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+        swapped = true;
+      }
+    }
+    if (!swapped) {
+      break;
+    }
+  }
+  return array;
 }
-console.log(sorting([ 2, 4, 6, 8, 2, 3 ])); // [ 2, 2, 3, 4, 6, 8 ]
+console.log(sorting([2, 4, 6, 8, 2, 3])); // [ 2, 2, 3, 4, 6, 8 ]
 function sortingByType(array) {
-  // your code here
+  let arrayType = [];
+  let arrayReturn = [];
+  for (let i = 0; i < array.length; i++) {
+    if (!arrayType.includes(typeof (array[i]))) {
+      arrayType.push(typeof (array[i]));
+    }
+  }
+  for (let i = 0; i < arrayType.length; i++) {
+    arrayReturn.push([]);
+  }
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < arrayReturn.length; j++) {
+      if (arrayReturn[j].length === 0) {
+        arrayReturn[j].push(array[i]);
+        break;
+      } else if (typeof (arrayReturn[j][0]) === typeof (array[i])) {
+        arrayReturn[j].push(array[i]);
+        break;
+      }
+    }
+  }
+  for (let i = 0; i < arrayReturn.length; i++) {
+    arrayReturn[i] = sorting(arrayReturn[i]);
+  }
+  return arrayReturn;
 }
-console.log(sortingByType([ 1, 3, 'array', -45, true, false, 'big' ]));
-// [ [ -45, 1, 3 ], [ 'array', 'big' ], [ false, true ] ]
+console.log(sortingByType([1, 3, 'array', -45, true, false, 'big'])); // [ [ -45, 1, 3 ], [ 'array', 'big' ], [ false, true ] ]
 function sortAllClean(array) {
-  //your code here
+  let limloop = array.length;
+  let count = 0;
+  for (let i = 0; i < limloop; i++) {
+    if (typeof (array[i - count]) === 'undefined' ||
+      array[i - count] === null ||
+      array[i - count].toString() === NaN.toString() ||
+      Array.isArray(array[i - count]) && (array[i - count].length === 0)) {
+      array.splice(i - count, 1);
+      count++;
+    }
+  }
+  return sortingByType(array.length === 1 ? array[0] === undefined ? [] : array : array);
 }
-console.log(sortAllClean([ undefined, null, 456, 'def', NaN, [], true, 123, 'bcd', false]));
-// [ [ 123, 456 ], [ 'bcd', 'def' ], [ false, true ] ]
-console.log(sortAllClean([ NaN, undefined ])); // []
+console.log(sortAllClean([undefined, null, 456, 'def', NaN, [], true, 123, 'bcd', false])); // [ [ 123, 456 ], [ 'bcd', 'def' ], [ false, true ] ]
+console.log(sortAllClean([NaN, undefined])); // []
